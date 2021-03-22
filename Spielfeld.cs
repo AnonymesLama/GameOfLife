0669abeb5f95abf08f68a9e5cc485c98dc2972a7
crescendolll_ <tensaemamo@gmail.com>
2021-03-22 22:57:51 +0100

    ansatz1 für spiellogik

--- Spielfeld.cs
+++ Spielfeld.cs
@@ -8,5 +8,33 @@ namespace GameOfLife
 {
     class Spielfeld
     {
+        public int generationenAnzahl;
+        private bool change = true;
+        private int cellCount;
+        //private int feldGroesse;
+        private Zelle [] kolonie;
+        private Zelle [] nextKolonie;
+
+        //nimmt Zellen an, setze jeweils nextstatus gemäß regeln
+        // speichert output in nextkolonie
+        // 
+        public void EvaluateNextGeneration()
+        {
+            //nextkolonie ist copy of kolonie
+
+            while (change)
+            {
+                foreach (Zelle spot in kolonie)
+                {
+                    //berechne anzahl neighbors
+                    //je nach anzahl, change state in nextkolonie
+                    //check ob change
+                    //if regeln erfüllt:
+                    spot.Evolve();
+                }
+            }
+        }
+
+
     }
 }
--- Zelle.cs
+++ Zelle.cs
@@ -8,5 +8,27 @@ namespace GameOfLife
 {
     class Zelle
     {
+        private bool status;
+        private bool nextStatus;
+
+        public bool Status
+        {
+            get { return this.status; }
+        }
+
+        public bool NextStatus
+        {
+            get { return this.nextStatus; }
+        }
+
+        //setter für status. nur bei initialisierung nutzen
+        public void StarterKolonie () { this.status = true; }
+
+        //setterfunktion für nextstatus
+        public void Evolve() { this.nextStatus = status; }
+
+
+
+
     }
 }
