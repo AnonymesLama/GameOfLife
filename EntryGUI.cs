8b7964e3cff36d7f97a967b590bec5fe7f2f9b12
AnonymesLama <merianduhla@gmail.com>
2021-03-24 09:09:29 +0100

    EntryGUI - Oberfläche fertig

---
+++ EntryGUI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    public partial class EntryGUI : Form
+    {
+        public EntryGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void EntryGUI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblInstructions_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
